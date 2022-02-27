250085d74f0b30ce71c0c939b8cba0363bd6d653
David Freerksen <dfreerksen@gmail.com>
2022-02-27 14:28:48 -0600

    ESLint cleanup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -16,13 +16,13 @@ const customConfig = {
       child_process: false
     }
   }
-};
+}
 
 environment.config.delete('node.dgram')
 environment.config.delete('node.fs')
 environment.config.delete('node.net')
 environment.config.delete('node.tls')
 environment.config.delete('node.child_process')
-environment.config.merge(customConfig);
+environment.config.merge(customConfig)
 
 module.exports = environment
